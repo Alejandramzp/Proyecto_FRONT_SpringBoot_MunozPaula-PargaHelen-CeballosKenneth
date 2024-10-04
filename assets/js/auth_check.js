b08deeb46095772210422cfbaa1510cd0b9f55e5
IKitzu <kennethsantiagoc@gmail.com>
2024-10-04 08:59:04 -0500

    test: :test_tube: Check sesiones - ¡ColorPop!

--- assets/js/auth_check.js
+++ assets/js/auth_check.js
@@ -1,4 +1,4 @@
-window.onload = function() {
+/* window.onload = function() {
     const loggedInUser = localStorage.getItem("loggedInUser");
     if (!loggedInUser) {
         alert("Debes iniciar sesión para acceder a esta página.");
@@ -10,3 +10,4 @@ function logout() {
     localStorage.removeItem("loggedInUser");
     window.location.href = "../../index.html"; // Redirige al login después del logout
 }
+ */
--- assets/js/login.js
+++ assets/js/login.js
@@ -2,15 +2,15 @@ function redirectToDashboard() {
     const username = document.getElementById('username').value;
     const password = document.getElementById('password').value;
 
-    if (username === "admin" && password === "admin123") {
-        sessionStorage.setItem('loggedInUser', 'admin');
-        window.location.href = "./assets/view/admin_dashboard.html";
-    } else if (username === "empleado" && password === "empleado123") {
-        sessionStorage.setItem('loggedInUser', 'empleado');
-        window.location.href = "./assets/view/empleado_dashboard.html";
-    } else if (username === "usuario" && password === "usuario123") {
-        sessionStorage.setItem('loggedInUser', 'usuario');
-        window.location.href = "./assets/view/usuario_dashboard.html";
+    if (username === "admin" && password === "admin") {
+/*         sessionStorage.setItem('loggedInUser', 'admin');
+ */        window.location.href = "./assets/view/admin_dashboard.html";
+    } else if (username === "gerente" && password === "gerente") {
+/*         sessionStorage.setItem('loggedInUser', 'empleado');
+ */        window.location.href = "./assets/view/gerente_dashboard.html";
+    } else if (username === "cajero" && password === "cajero") {
+/*         sessionStorage.setItem('loggedInUser', 'usuario');
+ */        window.location.href = "./assets/view/cajero_dashboard.html";
     } else {
         alert("Usuario o contraseña incorrectos. Inténtalo de nuevo.");
     }
