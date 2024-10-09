08d6b4ae8f6ab6751a531a70b5911fc2ea4f5d11
iKitzu <kennethsantiagoc@gmail.com>
2024-10-09 07:47:56 -0500

    perf: :zap: Mejora

--- assets/js/login.js
+++ assets/js/login.js
@@ -3,15 +3,19 @@ function redirectToDashboard() {
     const password = document.getElementById('password').value;
 
     if (username === "admin" && password === "admin") {
-/*         sessionStorage.setItem('loggedInUser', 'admin');
- */        window.location.href = "./assets/view/admin_dashboard.html";
+        // sessionStorage.setItem('loggedInUser', 'admin');
+        window.location.href = "./assets/view/admin_dashboard.html";
     } else if (username === "gerente" && password === "gerente") {
-/*         sessionStorage.setItem('loggedInUser', 'empleado');
- */        window.location.href = "./assets/view/gerente_dashboard.html";
+        // sessionStorage.setItem('loggedInUser', 'empleado');
+        window.location.href = "./assets/view/gerente_dashboard.html";
     } else if (username === "cajero" && password === "cajero") {
-/*         sessionStorage.setItem('loggedInUser', 'usuario');
- */        window.location.href = "./assets/view/cajero_dashboard.html";
+        // sessionStorage.setItem('loggedInUser', 'usuario');
+        window.location.href = "./assets/view/cajero_dashboard.html";
     } else {
-        alert("Usuario o contraseña incorrectos. Inténtalo de nuevo.");
+        Swal.fire({
+            icon: 'error',
+            title: 'Error',
+            text: 'Usuario o contraseña incorrectos. Inténtalo de nuevo.'
+        });
     }
 }
